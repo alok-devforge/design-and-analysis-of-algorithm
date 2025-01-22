8f90999a116939d101faaaec4cf4a02fdf5b94c1
Alok Kumar <140330466+ALOK-CST@users.noreply.github.com>
2025-01-22 19:46:32 +0530

    Refactor Merge Sort and Quick Sort implementations for improved readability

--- ASSIGNMENT 02/01_MergeSort.c
+++ ASSIGNMENT 02/01_MergeSort.c
@@ -1,60 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-void merge(int arr[], int start, int mid, int end)
+void merge(int arr[],int start,int mid,int end)
 {
-    int *temp = (int *)malloc((end - start + 1) * sizeof(int));
-    int left = start, right = mid + 1, index = 0;
-    while (left <= mid && right <= end)
+    int *temp= (int*)malloc((end-start+1)*sizeof(int));
+    int left= start,right= mid+1,index= 0;
+    while(left<=mid&&right<=end)
     {
-        if (arr[left] <= arr[right])
+        if(arr[left]<=arr[right])
         {
-            temp[index] = arr[left];
-            index++, left++;
+            temp[index]= arr[left];
+            index++,left++;
         }
         else
         {
-            temp[index] = arr[right];
-            index++, right++;
+            temp[index]= arr[right];
+            index++,right++;
         }
     }
-    while (left <= mid)
+    while(left<=mid)
     {
-        temp[index] = arr[left];
-        index++, left++;
+        temp[index]= arr[left];
+        index++,left++;
     }
-    while (right <= end)
+    while(right<=end)
     {
-        temp[index] = arr[right];
-        index++, right++;
+        temp[index]= arr[right];
+        index++,right++;
     }
-    index = 0;
-    while (start <= end)
+    index= 0;
+    while(start<=end)
     {
-        arr[start] = temp[index];
-        start++, index++;
+        arr[start]= temp[index];
+        start++,index++;
     }
     free(temp);
 }
-void mergeSort(int arr[], int start, int end)
+void mergeSort(int arr[],int start,int end)
 {
-    if (start == end)
+    if(start==end)
         return;
-    int mid = start + (end - start) / 2;
-    mergeSort(arr, start, mid);   // Left
-    mergeSort(arr, mid + 1, end); // Right
-    merge(arr, start, mid, end);
+    int mid= start+(end-start)/2;
+    mergeSort(arr,start,mid);   // Left
+    mergeSort(arr,mid+1,end); // Right
+    merge(arr,start,mid,end);
 }
 int main()
 {
     int n;
     printf("Enter the size of the array: ");
-    scanf("%d", &n);
+    scanf("%d",&n);
     int arr[n];
     printf("Enter the elements: ");
-    for (int x = 0; x < n; x++)
-        scanf("%d", &arr[x]);
-    mergeSort(arr, 0, n - 1);
+    for (int x= 0;x<n;x++)
+        scanf("%d",&arr[x]);
+    mergeSort(arr,0,n-1);
     printf("Sorted Array: ");
-    for (int x = 0; x < n; x++)
-        printf("%d ", arr[x]);
+    for (int x= 0;x<n;x++)
+        printf("%d ",arr[x]);
 }
--- ASSIGNMENT 02/02_QuickSort.c
+++ ASSIGNMENT 02/02_QuickSort.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-void swap(int *a, int *b)
+void swap(int *a,int *b)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
+    int t= *a;
+    *a= *b;
+    *b= t;
 }
 
-int partition(int arr[], int low, int high)
+int partition(int arr[],int low,int high)
 {
-    int pivot = arr[high];
-    int i = (low - 1);
-    for (int x = low; x <= high - 1; x++)
+    int pivot= arr[high];
+    int i= (low-1);
+    for(int x= low;x<=high-1;x++)
     {
-        if (arr[x] <= pivot)
+        if(arr[x]<=pivot)
         {
             i++;
-            swap(&arr[i], &arr[x]);
+            swap(&arr[i],&arr[x]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+    swap(&arr[i+1],&arr[high]);
+    return(i+1);
 }
-void quickSort(int arr[], int low, int high)
+void quickSort(int arr[],int low,int high)
 {
-    if (low < high)
+    if(low<high)
     {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        int pi= partition(arr,low,high);
+        quickSort(arr,low,pi-1);
+        quickSort(arr,pi+1,high);
     }
 }
 
@@ -35,15 +35,13 @@ int main()
 {
     int n;
     printf("Enter the size of the array: ");
-    scanf("%d", &n);
+    scanf("%d",&n);
     int arr[n];
     printf("Enter the elements: ");
-    for (int x = 0; x < n; x++)
-        scanf("%d", &arr[x]);
-    quickSort(arr, 0, n - 1);
+    for (int x= 0;x<n;x++)
+        scanf("%d",&arr[x]);
+    quickSort(arr,0,n-1);
     printf("Sorted array: ");
-    for (int x = 0; x < n; x++)
-    {
-        printf("%d ", arr[x]);
-    }
+    for (int x= 0;x<n;x++)
+        printf("%d ",arr[x]);
 }
